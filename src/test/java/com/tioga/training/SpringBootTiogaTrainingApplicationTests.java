b32b4b9204bab1c8f1bd172567325cdc2c50a0ae
Siddiqui <faiz.siddiqui@sap.com>
2021-12-15 18:31:29 +0530

    initial commit

---
+++ src/test/java/com/tioga/training/SpringBootTiogaTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tioga.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTiogaTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
