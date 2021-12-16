ff7d496c9f5ddb4c13d8ad4ae9a3dd25fa03b348
bheesham08 <bheeshamranoliya@gmail.com>
2021-12-16 20:10:22 +0530

    Adding reservation repository in Reservation Service Class

--- src/main/java/com/tioga/training/business/service/ReservationService.java
+++ src/main/java/com/tioga/training/business/service/ReservationService.java
@@ -3,18 +3,20 @@ package com.tioga.training.business.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tioga.training.data.repository.ReservationRepository;
 import com.tioga.training.data.repository.RoomRepository;
 
 @Service
 public class ReservationService {
 
 	private final RoomRepository roomRepository;
+	private final ReservationRepository reservationRepository;
 
 
     @Autowired
-    public ReservationService(RoomRepository roomRepository) {
+    public ReservationService(RoomRepository roomRepository,ReservationRepository reservationRepository) {
         this.roomRepository = roomRepository;
-
+		this.reservationRepository = reservationRepository;
     }
 
 
