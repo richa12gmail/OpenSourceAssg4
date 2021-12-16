b4127e911e85b56299d913ef849c0e574bc77973
bheesham08 <bheeshamranoliya@gmail.com>
2021-12-16 20:51:27 +0530

    Refactor

--- src/main/java/com/tioga/training/business/service/ReservationService.java
+++ src/main/java/com/tioga/training/business/service/ReservationService.java
@@ -28,8 +28,8 @@ private final GuestRepository guestRepository;
 
   @Autowired
   public ReservationService(RoomRepository roomRepository,GuestRepository guestRepository,ReservationRepository reservationRepository ) {
-      this.roomRepository = roomRepository;
-      this.guestRepository = guestRepository;
+    this.roomRepository = roomRepository;
+    this.guestRepository = guestRepository;
     this.reservationRepository = reservationRepository;
 
   }
